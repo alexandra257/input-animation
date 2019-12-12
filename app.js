b9f459e6de6cd7f3f792cead9540703aef1afdc9
Alexandra Nicolaides <alexandrajn17@gmail.com>
2019-12-12 12:08:37 +0000

    Input Animation Bar

---
+++ app.js
@@ -0,0 +1,39 @@
+{
+    margin: 0;
+    padding: 0;
+    box - sizing: border - box;
+}
+
+body{
+    height: 60vh;
+    display: flex;
+    justify - content: space - around;
+    align - items: center;
+    flex - direction: column;
+    font - family: sans - serif;
+}
+    
+    .form{
+    width: 50 %;
+    position: relative;
+    height: 50px;
+}
+    
+    .form input{
+    width: 100 %;
+    height: 100 %;
+    color: lightblue;
+    padding - top: 20px;
+    border: none;
+}
+    
+    .form label{
+    position: absolute;
+    bottom: 0px;
+    left: 0;
+    height: 100 %;
+    width: 100;
+    pointer - events: none;
+    border - bottom: 1px solid black;
+
+}
